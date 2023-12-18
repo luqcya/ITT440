fefbdeacc81ff68b81375a04afd4d15c2d36ca78
luqcya <37235502+luqcya@users.noreply.github.com>
2023-12-19 00:24:04 +0800

    deploy container
    python and mysql is deploy in container

---
+++ group-project/python-client/client.py
@@ -0,0 +1,24 @@
+import socket
+
+def get_latest_points(user):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        server_address = ('python-server', 5002)
+
+        client_socket.connect(server_address)
+
+        request = f"{user},update"
+        client_socket.send(request.encode('utf-8'))
+        
+        response = client_socket.recv(1024).decode('utf-8')
+        print(f"Server response: {response}")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        client_socket.close()
+
+if __name__ == "__main__":
+    get_latest_points("luqman")
